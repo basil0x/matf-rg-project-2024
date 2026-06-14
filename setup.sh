daebb33ce828b96bae0e331942714209e9864ba6
Owl <vasiljevicvasilije4@gmail.com>
2026-06-14 20:20:58 +0200

    setup.sh modified to work on my machine, test app compiles

--- setup.sh
+++ setup.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-sudo apt update
-sudo apt install pkg-config g++-13 clang-format clang-tidy cmake git build-essential libwayland-dev libxkbcommon-dev xorg-dev libgl1-mesa-dev mesa-common-dev mesa-utils doxygen graphviz libassimp-dev
-
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 10
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-13 10
-sudo update-alternatives --set cc /usr/bin/gcc
-sudo update-alternatives --set c++ /usr/bin/g++
-
-sudo update-alternatives --config gcc
-sudo update-alternatives --config g++
-
-sudo apt install python3-pip python3-venv -y
+sudo dnf install -y \
+  pkgconfig \
+  gcc \
+  g++ \
+  clang-tools-extra \
+  cmake \
+  git \
+  make \
+  automake \
+  wayland-devel \
+  libxkbcommon-devel \
+  libX11-devel \
+  libXrandr-devel \
+  libXi-devel \
+  libXcursor-devel \
+  libXinerama-devel \
+  mesa-libGL-devel \
+  mesa-libGLU-devel \
+  glx-utils \
+  doxygen \
+  graphviz \
+  assimp-devel \
+  python3-pip \
+  python3
 
 python3 -m venv .venv
 source ".venv/bin/activate" && pip3 install libclang
