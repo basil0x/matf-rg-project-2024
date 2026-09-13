36e4ce3af68f7e456119ca8aa55f8cfbc5916fce
Owl <vasiljevicvasilije4@gmail.com>
2026-09-13 20:27:38 +0200

    Added exiting upon pressing the ESCAPE key

--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -3,10 +3,20 @@
 //
 
 #include <MainController.hpp>
+
+#include "engine/platform/PlatformController.hpp"
+
 #include <spdlog/spdlog.h>
 
 namespace app {
     void MainController::initialize() {
         spdlog::info("Main Controller Initialized.");
     }
-}// namespace app
+    bool MainController::loop() {
+        auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+        if (platform->key(engine::platform::KeyId::KEY_ESCAPE).is_down()) {
+            return false;
+        }
+        return true;
+    }
+    }// namespace app
