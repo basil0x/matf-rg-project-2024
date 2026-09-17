5b317698cfe30cd3a4a732f33ecf6edadd774eca
Owl <vasiljevicvasilije4@gmail.com>
2026-09-17 17:03:21 +0200

    Added GUI Support For Lighting Controls

--- app/include/MainController.hpp
+++ app/include/MainController.hpp
@@ -5,6 +5,9 @@
 #ifndef MATF_RG_PROJECT_2024_MAINCONTROLLER_HPP
 #define MATF_RG_PROJECT_2024_MAINCONTROLLER_HPP
 
+#include "glm/vec3.hpp"
+
+
 #include <engine/core/Controller.hpp>
 
 namespace app {
@@ -14,17 +17,33 @@ class MainController: public engine::core::Controller {
     void initialize() override;
 
     bool loop() override;
-    void draw_cube();
+    void draw_bar();
+    void draw_gui();
     void draw() override;
     void begin_draw() override;
     void end_draw() override;
     void update_camera();
     void update() override;
 
+
+
+    //Lighting
+
+    glm::vec3 dirLightDirection{-0.2f, -1.0f, -0.3f};
+    glm::vec3 dirLightColor{1.0f, 1.0f, 1.0f};
+    float dirLightIntensity{1.0f};
+
+    glm::vec3 pointLightPos{0.0f, 5.0f, 0.0f};
+    glm::vec3 pointLightColor{1.0f, 0.8f, 0.4f};
+    float pointLightIntensity{1.0f};
+
 public:
     std::string_view name() const override {
         return "app:MainController";
     }
+
+    //Modes
+    bool interactMode{false};
 };
 }// namespace app
 
--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -8,6 +8,7 @@
 #include "engine/graphics/OpenGL.hpp"
 #include "engine/platform/PlatformController.hpp"
 #include "engine/resources/ResourcesController.hpp"
+#include "imgui.h"
 
 #include <spdlog/spdlog.h>
 
@@ -25,6 +26,7 @@ public:
     void MainController::initialize() {
         spdlog::info("Main Controller Initialized.");
         auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+        //platform->set_enable_cursor(false);
         platform->register_platform_event_observer(std::make_unique<MainPlatformEventObserver>());
         engine::graphics::OpenGL::enable_depth_testing();
     }
@@ -35,32 +37,57 @@ public:
         }
         return true;
     }
-    void MainController::draw_cube() {
+    void MainController::draw_bar() {
         // Model
         auto resources = engine::core::Controller::get<engine::resources::ResourcesController>();
         auto graphics = engine::core::Controller::get<engine::graphics::GraphicsController>();
-        engine::resources::Model * cube = resources->model("barLow");
-
-
+        engine::resources::Model * bar = resources->model("barLow");
 
 
         // Shader
 
-        engine::resources::Shader * shader = resources->shader("basic");
+        engine::resources::Shader * shader = resources->shader("lighting");
 
         shader->use();
         shader->set_mat4("projection",graphics->projection_matrix());
         shader->set_mat4("view",graphics->camera()->view_matrix());
+        shader->set_vec3("viewPos", graphics->camera()->Position);
+
+        shader->set_vec3("dirLightDirection", dirLightDirection);
+        shader->set_vec3("dirLightColor", dirLightColor);
+        shader->set_float("dirLightIntensity", dirLightIntensity);
+
+        shader->set_vec3("pointLightPos", pointLightPos);
+        shader->set_vec3("pointLightColor", pointLightColor);
+        shader->set_float("pointLightIntensity", pointLightIntensity);
+
+
         glm::mat4 model = glm::mat4(1.0f);
         model = glm::translate(model, glm::vec3(0.0f, 0.0f, -3.0f));
-        model = glm::scale(model, glm::vec3(0.5f));
+        model = glm::scale(model, glm::vec3(0.2f));
         shader->set_mat4("model",model);
 
 
-        cube->draw(shader);
+        bar->draw(shader);
+    }
+    void MainController::draw_gui() {
+            auto graphics = engine::core::Controller::get<engine::graphics::GraphicsController>();
+            graphics->begin_gui();
+            ImGui::Begin("Lighting");
+            ImGui::SeparatorText("Directional Light");
+            ImGui::DragFloat3("Direction", &dirLightDirection.x, 0.01f);
+            ImGui::ColorEdit3("Dir Color", &dirLightColor.x);
+            ImGui::SliderFloat("Dir Intensity", &dirLightIntensity, 0.0f, 3.0f);
+            ImGui::SeparatorText("Point Light");
+            ImGui::DragFloat3("Point Pos", &pointLightPos.x, 0.1f);
+            ImGui::ColorEdit3("Point Color", &pointLightColor.x);
+            ImGui::SliderFloat("Point Intensity", &pointLightIntensity, 0.0f, 3.0f);
+            ImGui::End();
+            graphics->end_gui();
     }
     void MainController::draw() {
-        draw_cube();
+        draw_bar();
+        draw_gui();
     }
     void MainController::begin_draw() {
         engine::graphics::OpenGL::clear_buffers();
@@ -79,7 +106,7 @@ public:
         float speed = 1.0f;
 
         if (platform->key(engine::platform::KeyId::KEY_LEFT_SHIFT).is_down()) {
-            speed = 2.5f;
+            speed = 3.5f;
         }
 
         if (platform->key(engine::platform::KeyId::KEY_W).is_down()) {
